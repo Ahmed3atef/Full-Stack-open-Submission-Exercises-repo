04c280c6c06028884a670402a99bed2f93bff6d4
Ahmed@Atef <47149282+Ahmed3atef@users.noreply.github.com>
2024-06-01 18:02:54 +0300

    2.2: Course information step7

--- Part2/courseInformation/src/component/Course.jsx
+++ Part2/courseInformation/src/component/Course.jsx
@@ -6,6 +6,7 @@ const Course = ({course}) => {
             {course.parts.map(part =>
                 <p key={part.id}>{part.name} {part.exercises}</p>
             )}
+            <b>total of {course.parts.reduce((accumulator, currentValue) => accumulator + currentValue.exercises, 0)} exercises</b>
         </div>
     )
 }
